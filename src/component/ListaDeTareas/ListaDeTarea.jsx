14b53058e5ec750a220607efaecae4d5fb51cb7f
Diego Panno <diegompanno@gmail.com>
2023-11-16 17:32:37 -0300

    Trabaje en la el estado de tarea completada antes de borrar

--- src/component/ListaDeTareas/ListaDeTarea.jsx
+++ src/component/ListaDeTareas/ListaDeTarea.jsx
@@ -1,58 +1,67 @@
-import { useState, useEffect } from "react"
-import Formulario from "../Fromulario/Formulario"
+
+import { useState, useEffect } from "react";
+import Formulario from "../Fromulario/Formulario";
 import Tarea from "../Tarea/Tarea";
 
-
-
 const obtenerDatos = () => {
-    const datos = localStorage.getItem('keyS');
-    try {
-      return JSON.parse(datos) || [];
-    } catch (error) {
-      console.error("Error al analizar datos:", error);
-      return [];
-    }
-  };
-
+  const datos = localStorage.getItem('keyS');
+  try {
+    return JSON.parse(datos) || [];
+  } catch (error) {
+    console.error("Error al analizar datos:", error);
+    return [];
+  }
+};
 
 const ListaDeTarea = () => {
-    const [tareas, setTareas] = useState(obtenerDatos());
-    
+  const [tareas, setTareas] = useState(obtenerDatos());
 
-    const agregarTarea = (nuevaTarea) => {
-        setTareas([...tareas, nuevaTarea]);
-       
-      };
+  const agregarTarea = (nuevaTarea) => {
+    setTareas([...tareas, nuevaTarea]);
+    // Actualiza el almacenamiento local
+    localStorage.setItem('keyS', JSON.stringify([...tareas, nuevaTarea]));
+  };
 
+  const eliminarTarea = (id) => {
+    const tareaActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareaActualizadas);
+    // Actualiza el almacenamiento local
+    localStorage.setItem('keyS', JSON.stringify(tareaActualizadas));
+  };
 
+  const completarTarea = (id) => {
+    const tareaActualizadas = tareas.map(tarea => {
+      if (tarea.id === id) {
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareaActualizadas);
+    // Actualiza el almacenamiento local
+    localStorage.setItem('keyS', JSON.stringify(tareaActualizadas));
+  };
 
-      const eliminarTarea = id => {
-        const tareaActualizadas = tareas.filter(tarea => tarea.id !== id);
-        setTareas(tareaActualizadas);
-        obtenerDatos(tareaActualizadas);
-      };  
-
-      useEffect(() => {
-        localStorage.setItem('keyS', JSON.stringify(tareas));
-      }, [tareas]);  
-
-
+  useEffect(() => {
+    localStorage.setItem('keyS', JSON.stringify(tareas));
+  }, [tareas]);
 
   return (
     <>
       <Formulario onSubmit={agregarTarea} />
       <div>
-        { tareas.map((tarea) =>
-        <Tarea
-          key={tarea.id}
-          id={tarea.id}
-          texto={tarea.texto}
-          eliminarTarea={eliminarTarea}
-        />
+        {tareas.map((tarea) =>
+          <Tarea
+            key={tarea.id}
+            id={tarea.id}
+            texto={tarea.texto}
+            completada={tarea.completada}
+            completarTarea={() => completarTarea(tarea.id)}
+            eliminarTarea={() => eliminarTarea(tarea.id)}
+          />
         )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ListaDeTarea
+export default ListaDeTarea;
--- src/component/Tarea/Tarea.jsx
+++ src/component/Tarea/Tarea.jsx
@@ -1,18 +1,18 @@
+
 import { AiOutlineDelete } from "react-icons/ai";
+import { BsCheck2 } from "react-icons/bs";
 import "./Tarea.css";
 
 const Tarea = (props) => {
-  const { id, texto, eliminarTarea } = props;
-
-  const handleEliminarTarea = () => {
-    eliminarTarea(id);
-  };
+  const { id, texto, completada, eliminarTarea, completarTarea } = props;
 
   return (
     <div className="conte-tarea">
-      <div className="tarea">
-        <div className="tarea-texto">✔️ {props.texto}</div>
-        <div className="tarea-borrar" onClick={handleEliminarTarea}>
+      <div className={completada ? 'tarea tarea-completada' : 'tarea'}>
+        <div className="tarea-texto" onClick={completarTarea}>
+          <BsCheck2 /> {texto}
+        </div>
+        <div className="tarea-borrar" onClick={eliminarTarea}>
           <AiOutlineDelete />
         </div>
       </div>
